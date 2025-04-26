8d9fdb82b8f0cb81009bd501e38c8c9c7b03eeb8
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2025-04-26 19:20:41 +0300

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-./build.sh mips
 ./build.sh arm
 ./build.sh x86
+# disabled mips cause for some reason all dynamic libraries are actually compiled as static-pie instead of dynamic which causes gcc to fail?
+# ./build.sh mips
